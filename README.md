402908d889276bceabb8e0270810f7b9d53721ea
CantCode <cantcode023@gmail.com>
2022-02-01 02:07:56 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+Hover your mouse to your left screen or right screen and you can see this little arrow showing up.
+
+Left arrow = slide to previous picture
+Next arrow = slide to next picture
+
+Check it out at https://cantcode023.github.io/ImageCarousel
+
+*IMPORTANT: MADE FOR FRONT END PRACTICING PURPOSES (mostly javascript)*
---
+++ main.js
@@ -0,0 +1,30 @@
+// get container class
+var container = document.getElementsByClassName('container')[0];
+// get img tag in container
+var img = container.getElementsByTagName('img')[0];
+// get first button in container
+var button = container.getElementsByTagName('button')[0];
+// get second button in container
+var button2 = container.getElementsByTagName('button')[1];
+
+// addEventListener to button
+button.addEventListener('click', function() {
+    let currentSrc = parseInt(img.src.split("/")[4].replace(".png", ""))
+    if (currentSrc == 1) {
+        currentSrc = 7
+        img.src = `images/${currentSrc-1}.png`
+    } else {
+        img.src = `images/${currentSrc-1}.png`
+    }
+})
+
+// addEventListener to button2
+button2.addEventListener('click', function() {
+    let currentSrc = parseInt(img.src.split("/")[4].replace(".png", ""))
+    if (currentSrc == 6) {
+        currentSrc = 0
+        img.src = `images/${currentSrc+1}.png`
+    } else {
+        img.src = `images/${currentSrc+1}.png`
+    }
+})
